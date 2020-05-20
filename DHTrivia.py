b06209277aa579e4ed6f04d5dd2bad8592647406
BillybobjonesFPV <63826042+BillybobjonesFPV@users.noreply.github.com>
2020-05-20 12:23:51 -0500

    Add files via upload

--- DHTrivia.py
+++ DHTrivia.py
@@ -13,6 +13,19 @@ cow_answers = [
 "45",
 "1611",
 "40"
+]
 
-
+dad_jokes = [
+"What's brown and sticky? \nA stick!",
+"Did you hear about the kidnapping at school? \nit's fine... he woke up",
+"How can you tell it's a dogwood tree? \nBy the bark",
+"Why is Peter Pan always flying? \nBecause he Neverlands",
+"Why did the coach go to the bank? \nTo get his quarterback",
+"Singing in the shower is fun until you get\nsoap in your mouth... \nThen it becomes a soap opera",
+"It takes guts to be an organ donor",
+"How does a penguin build is house?\nIgloos it together",
+"I told my doctor I heard buzzing. \nHe said it's just a bug going around",
+"What do you call a toothless bear? \nA gummy bear",
+"Two goldfish are in a tank. One says to the\nother, 'Do you know how to drive this thing?'",
+"Why did the scarecrow win the award? \nHe was outstanding in his field"
 ]
